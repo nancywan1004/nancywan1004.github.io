b0703d21c9122e12aad2e64c7eaf99faab962198
nancywan1004 <46666414+nancywan1004@users.noreply.github.com>
2019-02-03 20:51:46 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 
     var sidebar = $('#sidebar'),
         mask = $('.mask'),
+        backButton = $('.back-to-top'),
         sidebar_trigger = $('#sidebar_trigger');
 
     function ShowSideBar(){
@@ -17,5 +18,17 @@
     }
     sidebar_trigger.on('click', ShowSideBar);
     mask.on('click',HideSideBar);
+    backButton.on('click', function () {
+        $('html, body').animate({scrollTop: 0},800);
+    })
+
+    $(window).on('scroll',function(){
+        if($(window).scrollTop() > $(window).height())
+            backButton.fadeIn();
+        else
+            backButton.fadeOut();
+    })
+
+    $(window).trigger('scroll');
 
 })
